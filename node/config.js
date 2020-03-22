16c10098a67faf66c7df70da7ae533be1a4af605
MarcEtienneDartus <marcetienne.dartus@gmail.com>
2020-03-22 21:07:05 +0100

    getting config env

--- node/config.js
+++ node/config.js
@@ -2,14 +2,14 @@ const dotenv = require('dotenv');
 
 let envs = process.env;
 
-// if(process.env.ENVIRONMENT === 'dev'){
+if(process.env.ENVIRONMENT === 'dev'){
   const result = dotenv.config({ path: './.env'});
   if (result.error) {
     throw result.error;
   }
 
   envs = result.parsed;
-// }
+}
 
 
 module.exports = envs;
