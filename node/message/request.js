0d58e867fce2572a0bc42b0e5bd3a1b08b355291
Marc-Etienne Dartus <marcetienne.dartus@gmail.com>
2020-03-30 13:00:01 +0200

    Prod working

--- node/message/request.js
+++ node/message/request.js
@@ -89,7 +89,6 @@ const getMessageWithArgs = (intent, ...args) => {
 const recommend = (intent, desire, location) =>
   new Promise((resolve, reject) => {
     if (!desire) desire = "restaurant";
-    console.log(RECOMMEND_ENDPOINT)
     axios
       .post(RECOMMEND_ENDPOINT, {
         message: desire
